8af2a32186bf7e4b126f75d2721c196a4f07d4b1
Carlos David <apolayamendozacarlos@gmail.com>
2023-08-21 11:09:21 -0500

    Se crea la primera parte de la pagina proyectos

--- src/pages/Proyects.jsx
+++ src/pages/Proyects.jsx
@@ -1,6 +1,28 @@
+import { Link } from "react-router-dom"
+import MainHeader from "../component/MainHeader"
+import Botom from "../component/atm/Botom"
+
 const Proyects = () => {
   return(
-    <h1>545fd</h1>
+    <>
+      <MainHeader />
+      <div className="fondo py-20">
+        <div className="container mx-auto text-center">
+          <div className="mt-9">
+            <h1 className="text-3xl font-medium">
+              Explora mis proyectos web
+            </h1>
+            <div className="mt-14">
+              <Link to="/proyects">
+                <Botom name="Mira mis proyectos web" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Seccion de proyectos */}
+    </>
   )
 }
 
